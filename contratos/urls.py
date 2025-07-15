c64d417074a47cd7d6b59aab996ed5d1f6d75f2a
Pedro Antônio <133161673+pedroantonio-18@users.noreply.github.com>
2025-07-15 16:16:18 -0300

    Delete contratos directory

--- contratos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import VigenciaListCreateView, VigenciaDetailView
-
-urlpatterns = [
-    path('vigencias/', VigenciaListCreateView.as_view(), name='vigencia-list-create'),
-    path('vigencias/<int:pk>/', VigenciaDetailView.as_view(), name='vigencia-detail'),
-]
