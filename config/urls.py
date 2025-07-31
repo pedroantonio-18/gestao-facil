a3a37b504a7d1a63798e85a0aa9273e06d79c533
Gabryel Fernandes <gabryelfernandes67@gmail.com>
2025-07-31 15:33:28 -0300

    feat: adiciona tela de atualização de contratos, perfil e navegação entre elas
    Inclui as interfaces de atualização de contratos e perfil, além da navegação entre essas telas. Pendentes: integração com o banco de dados e criação da tela de exibição de contratos.

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-
-from pages.views import home
+from pages.views import home, profile, updateContracts  # <- importa daqui
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
+    path('perfil/', profile, name='profile'),
+    path('atualizar-contratos/', updateContracts, name='update_contracts'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "pages/home.html" )
+    return render(request, "pages/home.html")
+
+def profile(request):
+    return render(request, "pages/profile.html")
+
+def updateContracts(request):  # <- NOVA função, antes você tinha "profile" duplicado
+    return render(request, "pages/updateContracts.html")
