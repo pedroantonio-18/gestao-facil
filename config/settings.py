0c42295044d68de9dc5e62decd840f497111185f
Pedro Antônio <pedr18.a@gmail.com>
2025-08-20 10:49:40 -0300

    Alteração na Home - Novo Editor de Contratos

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'penalidades',
     'pagamentos',
     'pages.apps.PagesConfig',
-    "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -37,7 +36,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
--- config/urls.py
+++ config/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('', home, name='home'),
     path('perfil/', profile, name='profile'),
     path('atualizar-contratos/', updateContracts, name='update_contracts'),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
