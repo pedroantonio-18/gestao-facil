f8e00301c29171b028a589701b084c880af8eaba
Gabryel Fernandes <gabryelfernandes67@gmail.com>
2025-08-26 14:30:32 -0300

    mod

--- pages/static/js/scripts.js
+++ pages/static/js/scripts.js
@@ -1,11 +1,9 @@
 import { inputPages } from "./pagesInput.js";
 import { openModal } from "./modalNotifications.js";
-import { initContractEditor } from "./contractEditor.js";
 import { initContractEditorHome } from "./contractEditHome.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   openModal();
   inputPages();
-  initContractEditor();
   initContractEditorHome();
 });
